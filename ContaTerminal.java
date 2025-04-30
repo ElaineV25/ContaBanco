8ccf48c3ff81cd3f40b38ce749ac0f6c0f797416
ElaineV25 <69541767+ElaineV25@users.noreply.github.com>
2025-04-30 16:35:59 -0300

    Add files via upload

---
+++ ContaTerminal.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+
+public class ContaTerminal {
+    public static void mai(String[] args) throws Exception {
+        // Conhecer e importar a classe Scanner
+        Scanner scanner = new Scanner(System.in);
+
+        // Exibir as mensagens para o usuário e obter os valores digitados no terminal
+        System.out.println("Por favor, digite o número da Conta:");
+        int numeroConta = scanner.nextInt();
+        scanner.nextLine(); // Limpa o buffer do scanner
+
+        System.out.println("Por favor, digite o número da Agência:");
+        String agencia = scanner.nextLine();
+
+        System.out.println("Por favor, digite o nome do Cliente:");
+        String nomeCliente = scanner.nextLine();
+
+        System.out.println("Por favor, digite o saldo inicial:");
+        double saldo = scanner.nextDouble();
+
+        // Exibir a mensagem da conta criada
+        System.out.println("Olá " + nomeCliente + ", obrigado por criar uma conta em nosso banco, "
+                + "sua agência é " + agencia + ", conta " + numeroConta
+                + " e seu saldo R$" + String.format("%.2f", saldo) + " já está disponível para saque.");
+
+        scanner.close();
+    }
+}
+
+
